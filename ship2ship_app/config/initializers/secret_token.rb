508a658a3da459f2b22a2d41b3000bfa54bbeceb
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-23 15:08:11 -0400

    new rails app

---
+++ ship2ship_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ship2shipApp::Application.config.secret_key_base = '6159104ace42019e4d2dc2ea82801a3759ebb38dd572798ac41594df1e29051e56d317824ce74a35da8c16e4de2960eb0767730b8e33e14c4cfdf4975da70adf'
---
+++ ship2ship_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ship2shipApp::Application.config.session_store :cookie_store, key: '_ship2ship_app_session'
