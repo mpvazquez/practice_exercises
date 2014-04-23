f28f694733f79f5e34e507976f0312a520b387f9
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-04-23 15:17:30 -0400

    added routes and controller for messages

--- ship2ship_app/db/migrate/20140423190755_create_messages.rb
+++ ship2ship_app/db/migrate/20140423190755_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-
+      t.string :title
+      t.text :body
+      t.boolean :is_read, default: false
       t.timestamps
     end
   end
