4eb3290b032488d281d18124b1260fe57c9cd8f9
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-06-10 11:00:05 -0400

    evaluates two numbers for an exponent of two

--- increasing_numbers_alg/algorithm.rb
+++ increasing_numbers_alg/algorithm.rb
@@ -14,17 +14,23 @@ class Algorithm
 
   def evaluate_two(array)
     first_num = @array[0]
-    second_num = @array[1]
+    next_number = @array[1]
     following_numbers = []
 
-    if second_num % first_num == 0
-      divisor = second_num / first_num
+    if next_number % first_num == 0
+      divisor = next_number / first_num
       8.times do |i|
-        result = second_num * divisor
+        result = next_number * divisor
         following_numbers << result
-        second_num = result
+        next_number = result
       end
       puts following_numbers.to_s
+    elsif Math.sqrt(next_number) == first_num
+      8.times do |i|
+        result = next_number ** 2
+        following_numbers << result
+        next_number = result
+      end
     end
   end
 end
