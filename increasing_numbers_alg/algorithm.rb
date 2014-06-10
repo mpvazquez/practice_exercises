fe8532c6e701c7d704c2b262ba7f7d4b9c3c94c5
Marco Pretell-Vazquez <marco.pvazquez@gmail.com>
2014-06-10 10:52:53 -0400

    basic evaluation of two numbers where modulous of the second and first numbers is 0

--- increasing_numbers_alg/algorithm.rb
+++ increasing_numbers_alg/algorithm.rb
@@ -4,12 +4,28 @@ class Algorithm
   def initialize(nums)
     @array = []
     nums.each {|num| @array << num.to_i}
+
+    if @array.count == 2
+      evaluate_two(@array)
+    end
   end
 
   attr_accessor :array
 
-  def evaluate
+  def evaluate_two(array)
+    first_num = @array[0]
+    second_num = @array[1]
+    following_numbers = []
 
+    if second_num % first_num == 0
+      divisor = second_num / first_num
+      8.times do |i|
+        result = second_num * divisor
+        following_numbers << result
+        second_num = result
+      end
+      puts following_numbers.to_s
+    end
   end
 end
 
